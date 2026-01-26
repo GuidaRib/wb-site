1edf8d940538a48a7d3fb019e79f185e14773b66
GuidaRib <guida.ribeiro@whitebox.de>
2026-01-26 12:15:30 +0100

    feat: add last elements to wochenmarkt

--- app/components/Homepage/WochenMarkt.vue
+++ app/components/Homepage/WochenMarkt.vue
@@ -16,37 +16,55 @@
             </div>
 
      </div>
-       <div class="col-span-3 2xl:col-span-2 flex items-center ">
+       <div class="col-span-3 2xl:col-span-2 flex items-center relative z-0 ">
             <div ref="boxContent" class="bg-[url('/wm/wm_background.png')] bg-cover bg-center bg-no-repeat sm:h-[75vh] w-full  p-4">
                 <div ref="whiteBorder" class="flex flex-col lg:flex-row relative border-white border-2 h-full justify-between z-20">
-                <div class="w-full 2xl:w-1/2 h-full  max-h-56 lg:max-h-none relative z-20">
-                    <h2 ref="titleText" class="p-10 titleText text-5xl font-family-averBold text-white split max-w-3xs">MEINE WOCHENMÄRKTE BREMEN BREMERHAVEN</h2>
-                     <div class=" text-white gap-2 flex pl-10 w-1/2 lg:w-full">
-                        <div ref="mobileElement1" class="w-1/2">
-                            <div class="px-4">
-                                <div ref="textClp" class="text-base font-family-averRegular text-right" >SOCIAL Media</div>
-                                <div class="border-b-2 border-white"></div>
+                    <div class="w-full 2xl:w-1/2 h-full  max-h-56 lg:max-h-none relative z-20">
+                        <h2 ref="titleText" class="p-10 titleText text-5xl font-family-averBold text-white split max-w-3xs">MEINE WOCHENMÄRKTE BREMEN BREMERHAVEN</h2>
+                        <div class=" text-white gap-2 flex pl-10 w-1/2 lg:w-full">
+                            <div ref="mobileElement1" class="w-1/2">
+                                <div class="px-4">
+                                    <div ref="textClp" class="text-base font-family-averRegular text-right" >SOCIAL Media</div>
+                                    <div class="border-b-2 border-white"></div>
+                                </div>
+                                <img src="/wm/wm_mobile2.png" alt="">
                             </div>
-                            <img src="/wm/wm_mobile2.png" alt="">
-                        </div>
-                        <div ref="mobileElement2" class="w-1/2 mt-10 xl:mt-20">
-                            <div class="px-4">
-                                <div ref="textClp" class="text-base font-family-averRegular text-right ">MARKT finder</div>
-                                <div class="border-b-2  border-white"></div>
+                            <div ref="mobileElement2" class="w-1/2 mt-10 xl:mt-20">
+                                <div class="px-4">
+                                    <div ref="textClp" class="text-base font-family-averRegular text-right ">MARKT finder</div>
+                                    <div class="border-b-2  border-white"></div>
+                                </div>
+                                <img src="/wm/wm_mobile1.png" alt="">
                             </div>
-                            <img src="/wm/wm_mobile1.png" alt="">
                         </div>
                     </div>
+                    <div ref="laptopContainer" class=" lg:w-1/2 h-full ">
+                            <img src="/wm/wm_computer.png" class="w-full ml-auto sm:absolute  lg:w-[45vw] 2xl:w-[35vw] min-w-[500px] max-w-none bottom-0 2xl:-bottom-5 left-0 -right-40 lg:left-0 xl:left-10 relative z-20"  alt="">
+                    </div>
+                    <div ref="logoWM" class="absolute bottom-4 right-4 lg:right-10 z-30 ">
+                        <img src="/wm/wm_logo.png" alt="Wochenmarkt Logo" >
+
+                    </div>
                 </div>
-                <div ref="laptopContainer" class=" lg:w-1/2 h-full relative z-0">
-                        <img src="/wm/wm_computer.png" class="w-full ml-auto sm:absolute  lg:w-[45vw] 2xl:w-[35vw] min-w-[500px] max-w-none bottom-0 2xl:-bottom-5 left-0 -right-40 lg:left-0 xl:left-10"  alt="">
-                </div>
+                <div class="absolute w-full left-full top-0 col-span-1 items-center flex z-10 " >
+                    <div class="relative flex items-start gap-2 -ml-50  h-[75vh] z-0  w-full "> 
+                        <div ref="floatingTextContainer" class="px-4 pt-20 ">
+                            <div ref="floatingText" class="text-base text-white font-family-averRegular">Aktionen</div>
+                            <div class="border-b-2  border-white w-34"></div>
+                        </div>
+                        <div ref="floatingElements" class="flex flex-col gap-4 items-start absolute left-42 bottom-full -mb-60 w-full">
+                            <img ref="smallElement1" class="h-34 w-auto" src="/wm/wm_gluecksrad_2023.png" alt="">
+                            <img ref="smallElement2" class="h-34 w-auto" src="/wm/wm-az_2023.png" alt="">
+                            <img ref="smallElement3" class="h-34 w-auto" src="/wm/wm_e-auto_2024.png" alt="">
+                            <img ref="smallElement4" class="h-34 w-auto" src="/wm/wm_flaggen_2024.png" alt="">
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
 
-        <div class="col-span-1 items-center flex " >
-            <div class="relative  flex items-start gap-2 -ml-48  h-[75vh] z-10  w-full"> 
+   <!--      <div class="col-span-1 items-center flex relative z-0" >
+            <div class="relative  flex items-start gap-2 -ml-48  h-[75vh] z-0  w-full"> 
                 <div ref="floatingTextContainer" class="px-4 pt-20 ">
                     <div ref="floatingText" class="text-base text-white font-family-averRegular">Aktionen</div>
                     <div class="border-b-2  border-white w-30"></div>
@@ -58,7 +76,7 @@
                     <img ref="smallElement4" class="h-34 w-auto" src="/wm/wm_flaggen_2024.png" alt="">
                 </div>
             </div>
-        </div>
+        </div> -->
       
     </div>
     </section>
@@ -88,6 +106,7 @@ const smallElement3 = ref<HTMLElement | null>(null);
 const smallElement4 = ref<HTMLElement | null>(null);
 const floatingTextContainer = ref<HTMLElement | null>(null);
 const floatingText = ref<HTMLElement | null>(null);
+const logoWM = ref<HTMLElement | null>(null);
 
 let ctx: gsap.Context | null = null;
 
@@ -135,7 +154,7 @@ onMounted(() => {
         tl.set(mobileElement2.value, { yPercent: 200 });
         tl.set(floatingElements.value, { y: 0, yPercent: -100 });
         tl.set(floatingTextContainer.value, { autoAlpha: 0 });
-
+        tl.set(logoWM.value, { autoAlpha: 0, yPercent: -200 });
         tl.add("intro", 0);
 
     tl.fromTo(
@@ -147,7 +166,7 @@ onMounted(() => {
         yPercent: 0,
         skewX: 0,
         ease: "elastic.out(1,0.6)",
-        duration: 1.2
+        duration: 2
     },
     "intro"
     );
@@ -159,7 +178,7 @@ onMounted(() => {
         autoAlpha: 1,
         scale: 1,
         ease: "elastic.out(1,0.8)",
-        duration: 0.8
+        duration: 1.6
     },
     "intro+=0.4"
     );
@@ -168,7 +187,7 @@ tl.from(
   splitTitle.words,
   {
     autoAlpha: 0,
-    stagger: { amount: 0.1, from: "start" },
+    stagger: { amount: 0.3, from: "start" },
     ease: "power2.out",
     duration: 0.14
   },
@@ -179,9 +198,9 @@ tl.from(
   splitContent.words,
   {
     autoAlpha: 0,
-    stagger: { amount: 0.1, from: "start" },
+    stagger: { amount: 0.3, from: "start" },
     ease: "power2.out",
-    duration: 0.08
+    duration: 0.14
   },
   "intro+=1.2"
 );
@@ -202,14 +221,14 @@ tl.fromTo(
 tl.fromTo(
   mobileElement1.value,
   { yPercent: 0 },
-  { yPercent: -200, duration: 0.6, ease: "power4.in" },
+  { yPercent: -200, duration: 1, ease: "power4.in" },
   "intro+=4.8"
 );
 
 tl.fromTo(
   mobileElement2.value,
   { yPercent: 0 },
-  { yPercent: -300, duration: 0.6, ease: "power4.in" },
+  { yPercent: -300, duration: 1, ease: "power4.in" },
   "intro+=5.2"
 );
 
@@ -225,7 +244,7 @@ tl.fromTo(
         const STEP = smallElementHeight + gap;
         const textLabels = ['Aktionen', 'Anzeigen', 'Fahrzeugbeklebung', 'Werbemittel'];
 
-    tl.add("floating", "3.0");
+    tl.add("floating", "2.0");
 
     tl.fromTo(
     floatingTextContainer.value,
@@ -239,7 +258,7 @@ tl.fromTo(
     { yPercent: -100 },
     {
         yPercent: 0,
-        duration: 0.5,
+        duration: 0.6,
         onStart: () => {
         floatingText.value.textContent = textLabels[0];
         }
@@ -247,7 +266,7 @@ tl.fromTo(
     "floating"
     );
 
-    tl.addPause("+=0.5");
+    tl.addPause("+=0.2");
 
     tl.fromTo(
     floatingElements.value,
@@ -255,7 +274,7 @@ tl.fromTo(
     {
         y: STEP,
         yPercent: 0,
-        duration: 0.5,
+        duration: 0.6,
         onStart: () => {
         floatingText.value.textContent = textLabels[1];
         }
@@ -270,7 +289,7 @@ tl.fromTo(
     {
         y: STEP * 2,
         yPercent: 0,
-        duration: 0.5,
+        duration: 0.6,
         onStart: () => {
         floatingText.value.textContent = textLabels[2];
         }
@@ -285,7 +304,7 @@ tl.fromTo(
     {
         y: STEP * 3,
         yPercent: 0,
-        duration: 0.5,
+        duration: 0.6,
         onStart: () => {
         floatingText.value.textContent = textLabels[3];
         }
@@ -304,7 +323,7 @@ tl.fromTo(
     {
         y: STEP * 3,
         yPercent: 200,
-        duration: 0.5,
+        duration: 0.6,
         ease: "power1.out"
     }
     );
@@ -319,9 +338,9 @@ tl.fromTo(
         xPercent: 0,
         skewX: 0,
         ease: "power2.out",
-        duration: 0.6
+        duration: 1.2
     },
-    "intro+=4"
+    "intro+=5"
     );
 
     tl.fromTo(
@@ -334,7 +353,14 @@ tl.fromTo(
         ease: "power2.in",
         duration: 0.6
     },
-    "laptop+=1"
+    "laptop+=0"
+    );
+
+    tl.fromTo(
+    logoWM.value,
+    { autoAlpha: 0, yPercent: -200 },
+    { autoAlpha: 1, yPercent: 0, duration: 1.3, ease: "power2.out" },
+       "laptop+=0.2"
     );
 
 
